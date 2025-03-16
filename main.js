411fc940511a65a7238353c1a594d57bae361b97
babulpro <sh8657706@gmail.com>
2025-03-16 22:50:34 +0600

    division

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ function division(a, b) {
     return a / b;
 }
 sum(10, 2); // 5
+function sum(a, b) {
+    return a + b;
+}
+sum(5, 4); // 9
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(5, 4); // 20
